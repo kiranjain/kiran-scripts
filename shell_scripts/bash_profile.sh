33b88e9b5952435970314fb750c7732a4d495dcd
kiranjain <kirankjain@gmail.com>
2015-12-21 20:04:26 -0800

    Update bash_profile.sh

--- shell_scripts/bash_profile.sh
+++ shell_scripts/bash_profile.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
 export PATH=$PATH:~/bin
-export CLICOLOR=1
-export LSCOLORS=GxFxCxDxBxegedabagaced
+export CLICOLOR=1                            # telling mac osx to colorize the terminal
+export LSCOLORS=GxFxCxDxBxegedabagaced       # specifying color scheme for the ls command
+export GREP_OPTIONS='--color=auto'
 
+alias k="clear"
 alias j="cd .."
 alias p="pwd"
-alias l="ls -alGF"
 alias grep="grep --color=auto"
+alias l="ls -alGF"                           # colorized ls output listing all files and directories
+alias ld="ls -ld *"                          # listing only directories under the current directory
+alias lf="ls -alp | grep -v /"               # listing only files and excluding directories
+alias la="ls -ld .[^.]*"                     # listing only "hidden" files beginning with "." (dot)
+alias lsize="ls -asF | grep -v / | sort -rn" # list files in a dir by size and sort the output; not recursive
+alias skim="(head -5; tail -5) <"            # display and head and tail of a file
+
+# source separate aliases file if it exists
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# source separate functions file if it exists
+if [ -f ~/.bash_functions ]; then
+    . ~/.bash_functions
+fi
